09a99649f906609a17b2f25900fd487ded97aaa3
Akito-Shirai <tnbc080527@gmail.com>
2024-11-11 20:35:53 +0900

    Update Review03.java file

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -4,10 +4,10 @@ public class Review03 {
 
     public static void main(String[] args) {
         BaseBallTeam teamYakuruto = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
-        BaseBallTeam teamBaystars = new BaseBallTeam("横浜ベイスターズ", 73, 68, 2);
+        BaseBallTeam teamBaystars = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
         BaseBallTeam teamTigers = new BaseBallTeam("阪神タイガース", 68, 71, 4);
         BaseBallTeam teamGiants = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
-        BaseBallTeam teamCarp = new BaseBallTeam("広島カープ", 66, 74, 3);
+        BaseBallTeam teamCarp = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
         BaseBallTeam teamDragons = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);
 
         double yakurutoRate = teamYakuruto.getRate();
